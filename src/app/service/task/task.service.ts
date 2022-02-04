2c812ae6a79c8a0cbd93ac7c0d5fbb26aadcd2b7
unknown <brahim.ezzali@hotmail.com>
2022-02-04 20:32:27 +0100

    create Task service

---
+++ src/app/service/task/task.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient , HttpHeaders} from '@angular/common/http';
+import {Task} from '../Task';
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks";
+  constructor(private http:HttpClient) { }
+
+  getTasks() : Observable<Task[]>{
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+  deleteTaskById(id?:number) : Observable<Task>{
+    const url = `${this.apiUrl}/${id}`
+    return this.http.delete<Task>(url);
+  }
+}

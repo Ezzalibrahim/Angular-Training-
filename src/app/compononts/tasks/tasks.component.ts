ad0fd269cf046001d940159265a53ce82786943d
unknown <brahim.ezzali@hotmail.com>
2022-02-04 20:28:39 +0100

    create tasks componentto manage task

---
+++ src/app/compononts/tasks/tasks.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
+import {Task} from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks:Task[] = [];
+
+  constructor(private taskService:TaskService) { }
+
+  ngOnInit(): void {
+   this.taskService.getTasks().subscribe((tasks)=>{
+     this.tasks = tasks;
+   });
+  }
+
+  deleteTaskById(task: Task){
+    this.taskService.deleteTaskById(task.id).subscribe(()=>{
+      this.tasks = this.tasks.filter(_task => _task.id !== task.id);
+    })
+  }
+
+}

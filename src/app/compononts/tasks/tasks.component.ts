5ab911603d2166df21f084e3313868fb46930a89
unknown <brahim.ezzali@hotmail.com>
2022-02-05 13:09:40 +0100

    implement Add task methode and change reminder prop of task

--- src/app/compononts/tasks/tasks.component.ts
+++ src/app/compononts/tasks/tasks.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskService } from 'src/app/service/task.service';
+import { TaskService } from 'src/app/service/task/task.service';
+import { UiService } from 'src/app/service/ui/ui.service';
+import {Subscription} from 'rxjs';
 import {Task} from '../../Task';
 
 @Component({
@@ -9,9 +11,17 @@ import {Task} from '../../Task';
 })
 export class TasksComponent implements OnInit {
 
+  showAddTask : boolean = false;
+  subscription : Subscription = new Subscription();
   tasks:Task[] = [];
 
-  constructor(private taskService:TaskService) { }
+  constructor(private taskService:TaskService , 
+    private uiService : UiService) {
+      this.uiService.onToggle().subscribe(value => 
+        this.showAddTask = value
+      )
+      
+  }
 
   ngOnInit(): void {
    this.taskService.getTasks().subscribe((tasks)=>{
@@ -23,6 +33,20 @@ export class TasksComponent implements OnInit {
     this.taskService.deleteTaskById(task.id).subscribe(()=>{
       this.tasks = this.tasks.filter(_task => _task.id !== task.id);
     })
+  }
+
+  toggleTaskReminder(task : Task){
+    task.reminder = !task.reminder;
+    this.taskService.toggleTaskReminder(task).subscribe();
+  }
+  addTask(task : Task){
+    this.taskService.addTask(task).subscribe((task)=>{
+      this.tasks.push(task);
+    })
+  }
+
+  toggleTaskForm(){
+    this.uiService.toggleAdd();
   }
 
 }
